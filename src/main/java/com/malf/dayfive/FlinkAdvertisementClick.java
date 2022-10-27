c80ac9cedb4a799368275ad0e172977e9965c892
cutema <cutemalinfei@outlook.com>
2022-10-27 20:45:20 +0800

    keyBy 之后不能使用returns(),返回元组的类型信息。

--- src/main/java/com/malf/dayfive/FlinkAdvertisementClick.java
+++ src/main/java/com/malf/dayfive/FlinkAdvertisementClick.java
@@ -2,9 +2,12 @@ package com.malf.dayfive;
 
 import com.malf.bean.AdsClickLog;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+
 
 /**
  * 广告点击日志
@@ -22,7 +25,13 @@ public class FlinkAdvertisementClick {
                     return new AdsClickLog(Long.parseLong(strings[0]),Long.parseLong(strings[1]),strings[2],strings[3],Long.parseLong(strings[4]));
                 })
                 .map((MapFunction<AdsClickLog, Tuple2<Tuple2<String, Long>, Long>>) value -> Tuple2.of(Tuple2.of(value.getProvince(), value.getAdId()),1L))
-                .keyBy((KeySelector<Tuple2<Tuple2<String, Long>, Long>, Tuple2<String, Long>>) value -> value.f0)
+                .returns(Types.TUPLE(Types.TUPLE(Types.STRING,Types.LONG),Types.LONG))
+                .keyBy(new KeySelector<Tuple2<Tuple2<String, Long>, Long>, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> getKey(Tuple2<Tuple2<String, Long>, Long> value) {
+                        return value.f0;
+                    }
+                })
                 .sum(1)
                 .print();
 
